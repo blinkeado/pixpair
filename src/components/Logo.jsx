63577eccc43d5ed9dfa88060317bcb9d989e6866
Carlos P Jara <carlos@Carloss-iMac.local>
2025-05-12 20:04:12 -0500

    Update: Logo component styling

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ const Logo = () => (
   <img
     src="https://firebasestorage.googleapis.com/v0/b/pixpair.firebasestorage.app/o/crab5.svg?alt=media&token=93592dde-03d2-4140-bf36-c4cb0ca39f85"
     alt="PixCrab logo"
-    className="h-[34px] w-[34px]"
+    className="h-[64px] w-[64px]"
   />
 );
 
