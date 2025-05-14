eb8e8034d514d3809a0097fe734a40c5f9ef487e
Carlos P Jara <carlos@Carloss-iMac.local>
2025-05-13 19:28:47 -0500

    refactor: remove logo glow effect

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,9 +3,6 @@ import React from 'react';
 
 const Logo = () => (
   <div className="logo-container">
-    {/* this empty div renders the animated glow */}
-    <div className="logo-glow-effect" />
-    {/* this img sits on top */}
     <img
       src="https://firebasestorage.googleapis.com/v0/b/pixpair.firebasestorage.app/o/crab5.svg?alt=media&token=93592dde-03d2-4140-bf36-c4cb0ca39f85"
       alt="PixCrab logo"
